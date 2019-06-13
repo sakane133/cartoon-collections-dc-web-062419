d39cb16dbf5007ef6be6efd0c68935e597722f7c
Samantha Kane <s.kane133@gmail.com>
2019-06-13 03:11:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    if array.include?(cheese_types)
+    if array.include?(cheese_types) == true
       puts find_the_cheese(cheese_types)
     else
       array
