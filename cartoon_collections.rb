21de01131c7cee22921f6448ad29ad9d5ed0c0dc
Samantha Kane <s.kane133@gmail.com>
2019-06-13 02:52:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each do |list|
+    puts "#{dwarves.length +1} #{list}"
 end
 
 def summon_captain_planet# code an argument here
